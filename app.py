f1cd563d118669ea0a454ed79fec7a1f82e0dc05
Michael Carlson <mcarlsonr5@gmail.com>
2025-12-03 16:35:10 -0500

    in-class

--- app.py
+++ app.py
@@ -14,11 +14,22 @@ DYNAMODB_TABLE_NAME = os.environ.get('DYNAMODB_TABLE_NAME', '')
 @app.on_s3_event(bucket=S3_BUCKET, events=['s3:ObjectCreated:*'], suffix='.json')
 def s3_handler(event):
     # get the event, pull the file from s3, read it, and insert into DDB
-    pass
+    app.log.debug(f"Received event for bucket: {event.bucket} key: {event.key}")
+    # get the object from s3
+    data = get_s3_object(event.bucket, event.key)
+    # insert into DDB
+    insert_data_into_dynamodb(data)
+
+    return data
 
 def get_s3_object(bucket, key):
     # get the object from s3
-    pass
+    s3 = boto3.client('s3')
+    try:
+        response = s3.get_object(Bucket=bucket, Key=key)
+        return json.loads(response['Body'].read().decode('utf-8'))
+    except Exception as e:
+        print(e)
 
 def insert_data_into_dynamodb(data):
     dynamodb = boto3.resource('dynamodb')
@@ -42,4 +53,11 @@ def insert_data_into_dynamodb(data):
 @app.route('/access', methods=['GET'])
 def get_access():
     # return all records from DDB
-    pass
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(DYNAMODB_TABLE_NAME)
+    try:
+        items = table.scan()['Items']
+        sorted_items = sorted(items, key=lambda x: x['access_time'])
+        return sorted_items
+    except ClientError as e:
+        raise e
